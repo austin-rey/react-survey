344736d37481891b36b02acb961fc5cc895236ee
austin-rey <austinrey95@gmail.com>
2020-11-30 11:31:35 -0600

    Added seeder data to initial state

--- src/context/surveys/surveyState.js
+++ src/context/surveys/surveyState.js
@@ -13,23 +13,162 @@ const SurveyState = (props) => {
         questions: [
             {
                 id: 1,
-                title: "This is the title of the question 1",
+                title: "Please select your gender",
                 type: "Multiple Choice",
                 answers:[
                     {
                         id: 1,
-                        answerTitle: 'This is one answer'
+                        answerTitle: "Male"
                     },
                     {
                         id: 2,
-                        answerTitle: 'This is the second answer'
+                        answerTitle: "Gender"
                     },
                     {
                         id: 3,
-                        answerTitle: 'Oh shit, heres number 3'
+                        answerTitle: "Other"
                     }
                 ]
             },
+            {
+                id: 2,
+                title: "What is your highest education level completed?",
+                type: "Multiple Choice",
+                answers:[
+                    {
+                        id: 1,
+                        answerTitle: "No schooling completed"
+                    },
+                    {
+                        id: 2,
+                        answerTitle: "Some high school or diploma"
+                    },
+                    {
+                        id: 3,
+                        answerTitle: "High school graduate"
+                    },
+                    {
+                        id: 4,
+                        answerTitle: "Professional/Vocational/Technical Training"
+                    },
+                    {
+                        id: 5,
+                        answerTitle: "Bachelor’s degree"
+                    },
+                    {
+                        id: 6,
+                        answerTitle: "Master’s degree"
+                    },
+                    {
+                        id: 7,
+                        answerTitle: "Doctorate degree"
+                    }
+                ]
+            },
+            {
+                id: 3,
+                title: "What is your marital status?",
+                type: "Multiple Choice",
+                answers:[
+                    {
+                        id: 1,
+                        answerTitle: "Single/Never married"
+                    },
+                    {
+                        id: 2,
+                        answerTitle: "In a committed relationship"
+                    },
+                    {
+                        id: 3,
+                        answerTitle: "Married"
+                    },
+                    {
+                        id: 4,
+                        answerTitle: "Separated"
+                    },
+                    {
+                        id: 5,
+                        answerTitle: "Divorced"
+                    },
+                    {
+                        id: 6,
+                        answerTitle: "Spouse deceased"
+                    }
+                ]
+            },
+            {
+                id: 4,
+                title: "During the past 6 months, you have lived: (select all that is applicable)",
+                type: "Multiple Select",
+                answers:[
+                    {
+                        id: 1,
+                        answerTitle: "Alone"
+                    },
+                    {
+                        id: 2,
+                        answerTitle: "Parents"
+                    },
+                    {
+                        id: 3,
+                        answerTitle: "Friends/Roommates"
+                    },
+                    {
+                        id: 4,
+                        answerTitle: "Children"
+                    },
+                    {
+                        id: 5,
+                        answerTitle: "Partner"
+                    },
+                    {
+                        id: 6,
+                        answerTitle: "Other"
+                    }
+                ]
+            },
+            {
+                id: 5,
+                title: "How would you rate your mental health over the past 4 weeks?",
+                type: "Multiple Choice",
+                answers:[
+                    {
+                        id: 1,
+                        answerTitle: "Poor"
+                    },
+                    {
+                        id: 2,
+                        answerTitle: "Fair"
+                    },
+                    {
+                        id: 3,
+                        answerTitle: "Good"
+                    },
+                    {
+                        id: 4,
+                        answerTitle: "Very Good"
+                    },
+                    {
+                        id: 5,
+                        answerTitle: "Excellent"
+                    }
+                ]
+            },
+            {
+                id: 6,
+                title: "Are you taking this survey?",
+                type: "True/False",
+                answers:[
+                    {
+                        id: 1,
+                        answerTitle: "True"
+                    },
+                    {
+                        id: 2,
+                        answerTitle: "False"
+                    }
+                ]
+            }
         ]
     };
 
